a44eb09795301e94a999b9b2bb8713064ec32a3e
AlexandherCM <alexandher_1321124129@uptecamac.edu.mx>
2023-03-01 20:15:54 -0600

    Anexo de la clase Modelo

--- Scripts/SubirUsuario.php
+++ Scripts/SubirUsuario.php
@@ -1,13 +1,15 @@
 <?php 
-include("ConnBD.php");
+include("Clases/Modelo.php");
 include("Clases/clsSubirUsuario.php");
+
+$conexion = new Modelo();
 
 if(isset($_POST['SubirUsuario'])){
     $ObjBD = new clsSubirUsuario($_POST["Usuario"],$_POST["Correo"], $_POST["Contraseña"]); 
-    $ObjBD->SubirUsuario($conexion);  
+    $ObjBD->SubirUsuario($conexion->conectar());  
 
-    $_SESSION['mensaje'] = 'Usuario Guardado satisfactoriamente';
-    $_SESSION['color'] = 'success';
+    // $_SESSION['mensaje'] = 'Usuario Guardado satisfactoriamente';
+    // $_SESSION['color'] = 'success';
 }
 
 ?>
